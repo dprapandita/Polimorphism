7d6536db406c8a061c91778873db4aa80970ba39
dprapandita@gmail.com <cybersecure751@gmail.com>
2026-04-02 18:18:38 +0700

    Updated example

--- Program.cs
+++ Program.cs
@@ -9,3 +9,23 @@ Ratu ratu = new Ratu();
 Console.WriteLine($"\nCatur: {ratu.Name}");
 ratu.Berjalan();
 ratu.Makan();
+
+Benteng benteng = new Benteng();
+Console.WriteLine($"\nCatur: {benteng.Name}");
+benteng.Berjalan();
+benteng.Makan();
+
+Pion pion = new Pion();
+Console.WriteLine($"\nCatur: {pion.Name}");
+pion.Berjalan();
+pion.Makan();
+
+Menteri menteri = new Menteri();
+Console.WriteLine($"\nCatur: {menteri.Name}");
+menteri.Berjalan();
+menteri.Makan();
+
+Kuda kuda = new Kuda();
+Console.WriteLine($"\nCatur: {kuda.Name}");
+kuda.Berjalan();
+kuda.Makan();
